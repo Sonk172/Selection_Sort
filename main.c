f7b0c0daa4662dd4ad374b417ce3617abfe13030
Sonk172 <daniel.slachov@gmail.com>
2023-01-08 11:50:43 +1100

    Create main.c
    Setup the workspace

---
+++ main.c
@@ -0,0 +1,14 @@
+// Selection Sort
+//
+// Written by Daniel Slachov
+// on 8/01/2023
+//
+// Sorts an array using the selection algorithm
+
+#include <stdio.h>
+
+int main(void) { 
+  
+  
+  return 0; 
+}
